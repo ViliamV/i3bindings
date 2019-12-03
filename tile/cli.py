2adc7babd4bb819435bed80c89425bd04d559e2f
Viliam Valent <vilo.valent@gmail.com>
2019-12-03 01:11:38 +0100

    Better CLI help

--- tile/cli.py
+++ tile/cli.py
@@ -9,33 +9,38 @@ from .tree_algorithms import parse
 
 def main():
     parser = argparse.ArgumentParser(epilog="Documentation: https://github.com/ViliamV/tile")
-    parser.add_argument("infile", metavar="input_file", type=argparse.FileType("r"), nargs="?", default=sys.stdin)
     parser.add_argument(
-        "-c",
+        "infile",
+        metavar="input_file",
+        type=argparse.FileType("r"),
+        nargs="?",
+        default=sys.stdin,
+        help="if not provided, defaults to stdin",
+    )
+    parser.add_argument(
         "--config",
         metavar="config_path",
         type=argparse.FileType("r+"),
-        help="Custom config path. Only applicable with '--write'",
+        help="overwrite config path, only applicable with '--write'",
     )
     parser.add_argument(
-        "--sway",
-        action="store_true",
-        help="Don't use '--no-startup-id' in exec binding. When used together with '--write', use default sway config file.",
+        "--sway", action="store_true", help="don't use '--no-startup-id', use sway config file if '--write'",
     )
     output_group = parser.add_mutually_exclusive_group()
-    output_group.add_argument("--write", action="store_true", help="Write output directly to i3 config file")
+    output_group.add_argument("--write", action="store_true", help="write output directly to i3 config file")
     output_group.add_argument(
-        "-o",
         "--output",
         dest="outfile",
         metavar="output_file",
         type=argparse.FileType("w"),
-        help="Defaults to print to stdout",
+        help="defaults to print to stdout",
         default=sys.stdout,
     )
     parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}")
 
     args = parser.parse_args()
+    if args.infile is sys.stdin:
+        print("[Interactive mode] Use Ctrl+D to exit")
     if args.write:
         if args.config:
             config = args.config
@@ -43,7 +48,7 @@ def main():
             config_path = pathlib.Path.home() / f".config/{'sway' if args.sway else 'i3'}/config"
             if not config_path.is_file():
                 print(f"tile: error: No such file: '{config_path}'")
-                print("Hint: add option '--config config_path'")
+                print("hint: add option '--config config_path'")
                 sys.exit(1)
             config = config_path.open("r+")
         to_config(config, parse(from_file(args.infile), sway=args.sway))
